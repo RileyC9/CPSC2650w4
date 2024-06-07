e5ee69b07c4ac12c0bd2bfaef758c976ef3e3371
Riley Cheung <tcheung25@mylangara.ca>
2024-06-06 17:20:11 -0700

    Finished mongoose exercise.

---
+++ mongoose_test.js
@@ -0,0 +1,193 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+
+async function connectToDatabase() {
+  const uri = process.env.MONGO_URI;
+  const dbNameURL = process.env.MONGODBNAME;
+  if (!uri) {
+    throw new Error("Missing MONGOURI environment variable");
+  }
+
+  await mongoose.connect(uri, {dbName: dbNameURL});
+}
+
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  year: { type: Number, required: true },
+  director: { type: String, required: true },
+  genre: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Movie = mongoose.model("movies", movieSchema);
+
+async function getOneMovie() {
+  try {
+    const movies = await Movie.findOne({});
+    console.log("A movie", movies);
+  } catch (err) {
+    console.error("Error fetching movies:", err.message);
+  }
+}
+
+async function main() {
+  try {
+    await connectToDatabase();
+    await getOneMovie();
+  } catch (err) {
+    console.log(`It blew up! ${err}`);
+    process.exit(1);
+  } finally {
+    mongoose.connection.close();
+  }
+}
+
+await main();
+
+const e2a = async () => {
+  try {
+    await connectToDatabase();
+    const result = await Movie
+    .find({ year: { $gt:2000 } });
+    return result;
+  } catch (err) {
+    console.log(`It blew up! ${err}`);
+    process.exit(1);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+const e2b = async () => {
+  // all distinct languages in all movies
+  try {
+    await connectToDatabase();
+    const result = await Movie
+    .distinct("languages", { type: { $regex: "movie"}});
+    return result;
+  } catch (err) {
+    console.log(`It blew up! ${err}`);
+    process.exit(1);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+const e2c = async () => {
+  // All movie reated PG-13 and the casting included Ryan Gosling
+  try {
+    await connectToDatabase();
+    const result = await Movie
+    .find( {$and: [{rated: { $regex: "PG-13" }}, {cast: { $regex: "Ryan Gosling"}}]})
+    .sort({released: 'asc'});
+    return result;
+  } catch (err) {
+    console.log(`It blew up! ${err}`);
+    process.exit(1);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+const e2d = async () => {
+  // Number of movies per gerne
+  try {
+    await connectToDatabase();
+    const result = await Movie
+    .aggregate([ { $match: { type: "movie"} } ])
+    .unwind("genres")
+    .group( { _id: "$genres", totalQuantity: { $sum: 1 } } );
+    return result;
+  } catch (err) {
+    console.log(`It blew up! ${err}`);
+    process.exit(1);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+const e2e = async () => {
+  // Number of movies per gerne
+  try {
+    await connectToDatabase();
+    const result = await Movie
+    .insertMany(
+      {
+        title: "CPSC2650",
+        year: 2000,
+        director: "Ting Hin Cheung",
+        genre: "Crime"
+      }
+    )
+    return result;
+  } catch (err) {
+    console.log(`It blew up! ${err}`);
+    process.exit(1);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+const e2f = async () => {
+  // Number of movies per gerne
+  try {
+    await connectToDatabase();
+    const result = await Movie
+    .insertMany(
+      {
+        title: "CPSC2650",
+        year: "whatever",
+        director: "Ting Hin Cheung",
+        genre: "Crime"
+      }
+    )
+    return result;
+  } catch (err) {
+    console.log(`It blew up! ${err}`);
+    process.exit(1);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+const e2g = async () => {
+  try {
+    await connectToDatabase();
+    const result = await Movie
+    .find( { director: { $regex: "Ting Hin Cheung" } } );
+    return result;
+  } catch (e) {
+    console.log(`It blew up! ${e}`);
+  } finally {
+    mongoose.connection.close();
+  }
+}
+
+// f. Now try inserting a movie with the year: "whatever", with {your_name} as the Director. Did that work? Take a screenshot of the error.
+
+// g. Now change the year schema from { type: Number } => year: { type: String }. Add the year: "whatever" movie again. Keeping the year: {type: String} schema above, write a query to get all movies where {your_name} is the Director. What happened to the original movie that was in year: 2000? Take a screenshot
+
+// Note: you do not need to write a new function for e2f. Use the same insertion code as in e2e.
+
+// h. Before you submit, make sure to revert the schema back so that the year is a Date.
+
+// console.log(`All movies after year 2000 ----->`);
+// console.log( await e2a());
+
+// console.log(`All distinct languages in all movies ----->`);
+// console.log(await e2b());
+
+// console.log(`All distinct languages in all movies ----->`);
+// console.log(await e2c());
+
+// console.log(`Number of movies per gerne ----->`);
+// console.log(await e2d());
+
+// console.log(`Insert a movie with CPSC2650 as title and director as myself (Ting Hin Cheung) ----->`);
+// console.log(await e2e());
+
+// console.log(`Insert a movie with CPSC2650 as title and director as myself (Ting Hin Cheung) with 'whatever' in year ----->`);
+// console.log(await e2f());
+
+// console.log(`Insert a movie with CPSC2650 as title and director as myself (Ting Hin Cheung) with 'whatever' in year ----->`);
+// console.log(await e2g());
